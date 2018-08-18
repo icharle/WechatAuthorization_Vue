7ee7c837cff7a0e332ed4eb45502822a49b5844e
icharle <icharle@live.com>
2018-08-18 22:43:39 +0800

    添加扫码页面

---
+++ src/pages/scan/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button type="primary">登录</button>
+    <button type="warn">取消</button>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+      }
+    },
+    onLoad () {
+      let options = this.$root.$mp.query
+      console.log(options.scene)
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
